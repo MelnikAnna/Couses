4dbc60023a1b40b24d834b01ee6728775ba511de
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2023-07-05 23:31:29 +0300

    added homework from the third lesson

---
+++ third_lesson/test.js
@@ -0,0 +1,31 @@
+let a = 0;
+// while (a < 5) {
+//   a++;
+//   console.log(a);
+// }
+
+// do {
+//   console.log(a);
+//   a--;
+// } while (a > 0);
+
+// for (; a < 5; ) {
+//   a++;
+//   console.log(a);
+// }
+
+// while (true) {
+//   let value = +prompt("enter: ");
+//   if (!value) break;
+//   sum += value;
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   if (i % 2 === 0) continue;
+//   console.log(i);
+// }
+
+let arr = ["apple", "orange", "plum"];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
